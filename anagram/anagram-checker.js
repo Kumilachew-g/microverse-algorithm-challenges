fab63339900bd3d7789c25dbe293bc4fc9f6439e
kumilachew-g <kumilachew1212@gmail.com>
2022-10-10 15:33:27 +0300

    Enhancement

--- anagram/anagram-checker.js
+++ anagram/anagram-checker.js
@@ -1,17 +1,17 @@
 module.exports = function (str1, str2) {
-  let s1 = str1
+  let string1 = str1
     .toLowerCase()
     .split("")
     .filter((k) => k >= "a" && k <= "z")
     .sort()
     .join();
-  let s2 = str2
+  let string2 = str2
     .toLowerCase()
     .split("")
     .filter((k) => k >= "a" && k <= "z")
     .sort()
     .join();
-  if (s1 === s2) {
+  if (string1 === string2) {
     return true;
   } else {
     return false;
